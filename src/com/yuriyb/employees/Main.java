bc0e47bded6afb795951afbe63f455527d102e76
YuriyB <yuriybpost@gmail.com>
2015-04-16 06:05:45 +0200

    added catch blocks in Main.java

--- src/com/yuriyb/employees/Main.java
+++ src/com/yuriyb/employees/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	// Exceptions handling should be present
 	// Logger should be used
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+	public static void main(String[] args) {
 
 		String fileName = "C://file.dat";
 		List<Employee> collection = new ArrayList<Employee>();
@@ -24,8 +24,23 @@ public class Main {
 		collection.add(new ShiftEngineer("Robin Good", 15.00));
 		collection.add(new ShiftEngineer("Hary Potter", 13.00));
 		
-		rwe.writeEmployeesIntoFile(collection, fileName);
-		rwe.readEmployeesFromFile(fileName);
+		try {
+			rwe.writeEmployeesIntoFile(collection, fileName);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			rwe.readEmployeesFromFile(fileName);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
 		
 		for (Employee e: collection)
 			System.out.println(e.getName()+" "+e.getSalary());
@@ -45,6 +60,7 @@ public class Main {
 class ComparisonByWageName implements Comparator<Employee>{
 	
 	public int compare(Employee one, Employee two){
+		
 		if (one.getSalary()==two.getSalary()){
 			return one.getName().compareTo(two.getName());
 		}
